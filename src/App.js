885a44dd45156d58ca80d47c9d15ccf223d4bf37
khushi11saxena <103021512+khushi11saxena@users.noreply.github.com>
2023-12-04 12:13:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 function App() {
+  const [mode, setmode] = useState('light');//tell about dark mode enable or not
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setmode('dark');
+     document.body.style.backgroundColor = '#2F2E2E';
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor = '#fff';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Navbar title="TextoConv" mode={mode} toggleMode={toggleMode}/>
+     <div className="container my-3">
+     <TextForm heading="Enter the text to analyze below" mode={mode}/>
+     </div>
+    </>
+    
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,96 @@
+import React ,{useState} from 'react'
+
+export default function TextForm(props) {
+    const handleup = ()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handlelo = ()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleclr = ()=>{
+        setText('');
+    }
+    const handleinv = ()=>{
+        //console.log("Uppercase was clicked" + text);
+        let reversedString = text.split("").reduce((acc, char) => char + acc, "");
+        setText(reversedString);
+    }
+    const capatalize = ()=>{
+        //console.log("Uppercase was clicked" + text);
+       let newText= text.toLowerCase().split(" ");
+        let finalstr="";
+        for(var i=0;i<newText.length;i++)
+        {
+            finalstr=finalstr+" "+ newText[i].charAt(0).toUpperCase()+newText[i].substring(1);
+        }
+        setText(finalstr);
+    }
+    const titlecase = ()=>{
+        let newstrs = text.split(".");
+        let finalstr="";
+        for(var i=0;i<newstrs.length;i++)
+        {
+            finalstr=finalstr+"."+ newstrs[i].substring(0,2).toUpperCase()+newstrs[i].substring(3);
+        }
+        setText(finalstr.substring(1));
+        
+    }
+   
+    const altering = ()=>{
+        //console.log("Uppercase was clicked" + text);
+            var chars = text.toLowerCase().split("");
+            for (var i = 0; i < chars.length; i += 2) {
+            chars[i] = chars[i].toUpperCase();
+            }
+            setText(chars.join(""));
+            
+    }
+    const handlecopy = ()=>{
+        var text=document.getElementById("mybox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleremext = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+   
+    const handleonChange = (event)=>{
+        //console.log("On change");
+        setText(event.target.value);
+    }
+    //Important 
+    const [text, setText] = useState('');
+   
+  return (
+    <>
+    <div className="container mb-1 " style={{color:props.mode==='light'?'black':'white'}} >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleonChange} style={{backgroundColor:props.mode==='dark'?'#494545':'white',color:props.mode==='dark'?'white':'black',borderColor:props.mode==='dark'?'black':'#dadada'}} id="mybox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleup}>Uppercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handlelo}>Lowercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleinv}>Inverse</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={capatalize}>Capatalize Case</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={titlecase}>Title Case</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={altering}>Altering Case</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleremext}>Remove extraspaces</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handlecopy}>Copy to clipboard</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleclr}>Clear text</button>
+        
+    </div>
+    <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length}characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter text to preview"}</p>
+    </div>
+    </>
+  )
+}
